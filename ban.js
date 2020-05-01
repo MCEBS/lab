cdac420616b4135fa311d835a4a94ab5a7879e59
fly6022 <2875752791@qq.com>
2020-05-01 14:32:32 +0800

    更新网站

--- ban.js
+++ ban.js
@@ -5,9 +5,11 @@ function search() {
     
     var search_value = document.getElementById('ban').value;
 
-    document.getElementById("ban").innerHTML ="<font color='red'><b>查询失败！</b></font>请检查您输入的信息是否正确或者该信息从未录入。";
+    var json
 
-    window.onload = function() {
+    var Box = document.getElementById("ban")
+
+    window.onload = function blacklist_json () {
         var url = "blacklist.json"
         var request = new XMLHttpRequest();
         request.open("get", url);
@@ -15,19 +17,41 @@ function search() {
         request.onload = function() {
             if (request.status == 200) {
                 json = JSON.parse(request.responseText);
-                switch (search_value) {
-            
-                    case '0':
-                    case 'test':
-                        json = JSON.parse(request.responseText)
-                        document.getElementById("ban").innerHTML = "<b><font color='SpringGreen'>查询成功！</font></b>" + "<br>" + "<b>BAN ID:</b>" + json.blacklist[0].id + "<br>" + "<b>QQ号:</b>" + json.blacklist[0].qq + "<br>" + "<b>记录日期:</b>" + json.blacklist[0].date + "<br>" + "<b>有效期至:</b>" + json.blacklist[0].validity + "<br>" + "<b>云黑名单等级:</b>" + json.blacklist[0].level + "<br>" + "<b>事件记录:</b>" + json.blacklist[0].note + "<font color='red'><b>【重度违规，建议提高警惕】</b></font>" + "<br>" + "<b>证实情况:</b>" + json.blacklist[0].confirm + "<br>" + "<b>证据链接:</b>" + json.blacklist[0].evidence + "<br>" + "<b>查询结果来自:</b>" + json.blacklist[0].from;
-                        break;
-                        
-                    }
-                location = url;
+                showData(json)
                 }
             }
         }
+
+        function showData(data) {
+
+            for (let i = 0; i < json.blacklist.length; i++) {
+    
+                var blacklist = json.blacklist[i].id
+    
+                console.log(blacklist);
+    
+                var p = document.createElement("p")
+    
+                p.innerHTML = gameName
+    
+                Box.appendChild(p)
+    
+            }
+    
+        }
+
+
+    switch (search_value) {
+            
+            case '0':
+            case 'test':
+                document.getElementById("ba").innerHTML = "<b><font color='SpringGreen'>查询成功！</font></b>" + "<br>" + "<b>BAN ID:</b>" + json.blacklist[0].id + "<br>" + "<b>QQ号:</b>" + json.blacklist[0].qq + "<br>" + "<b>记录日期:</b>" + json.blacklist[0].date + "<br>" + "<b>有效期至:</b>" + json.blacklist[0].validity + "<br>" + "<b>云黑名单等级:</b>" + json.blacklist[0].level + "<br>" + "<b>事件记录:</b>" + json.blacklist[0].note + "<font color='red'><b>【重度违规，建议提高警惕】</b></font>" + "<br>" + "<b>证实情况:</b>" + json.blacklist[0].confirm + "<br>" + "<b>证据链接:</b>" + json.blacklist[0].evidence + "<br>" + "<b>查询结果来自:</b>" + json.blacklist[0].from;
+                break;
+
+            default:
+                document.getElementById("ba").innerHTML ="<font color='red'><b>查询失败！</b></font>请检查您输入的信息是否正确或者该信息从未录入。";
+
+    }
 }
 
 // 统计插件
