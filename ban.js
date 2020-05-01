39619dcca4713965efdbe1c1843c3506009b2ec3
fly6022 <2875752791@qq.com>
2020-05-01 12:51:47 +0800

    更新网站

--- ban.js
+++ ban.js
@@ -9,28 +9,30 @@ function search() {
     
     // data.json 数据库
 
-	window.onload = function () {
-        var url = "blacklist.json" /*json文件url，本地的就写本地的位置，如果是服务器的就写服务器的路径*/
+
+    window.onload = function() {
+        var url = "blacklist.json"
         var request = new XMLHttpRequest();
-        request.open("GET", url); /*设置请求方法与路径*/
-        request.send(null); /*不发送数据到服务器*/
-        request.onload = function () { /*XHR对象获取到返回信息后执行*/
-            if (request.status == 200) { /*返回状态为200，即为数据获取成功*/
-                 var json = JSON.parse(request.responseText);
-                for(var i=0;i<json.length;i++){
-                    console.log(json[i].name);
-                }
-                console.log(json);
+        request.open("get", url);
+        request.send(null);
+        request.onload = function() {
+            if (request.status == 200) {
+                var text = JSON.parse(request.responseText);
             }
         }
     }
+    setTimeout(function() { 
+        console.log(text);
+    }, 1000)
+
     
     // 云端黑名单总控系统
     switch(search_value) {
             
         case '0':
         case 'test':
-            obj = JSON.parse(request.responseText); document.getElementById("ban").innerHTML = "<b><font color='SpringGreen'>查询成功！</font></b>" + "<br>" + "<b>BAN ID:</b>" + obj.blacklist[0].id + "<br>" + "<b>QQ号:</b>" + obj.blacklist[0].qq + "<br>" + "<b>记录日期:</b>" + obj.blacklist[0].date + "<br>" + "<b>有效期至:</b>" + obj.blacklist[0].validity + "<br>" + "<b>云黑名单等级:</b>" + obj.blacklist[0].level + "<br>" + "<b>事件记录:</b>" + obj.blacklist[0].note + "<font color='red'><b>【重度违规，建议提高警惕】</b></font>" + "<br>" + "<b>证实情况:</b>" + obj.blacklist[0].confirm + "<br>" + "<b>证据链接:</b>" + obj.blacklist[0].evidence + "<br>" + "<b>查询结果来自:</b>" + obj.blacklist[0].from;
+            obj = JSON.parse(text); 
+            document.getElementById("ban").innerHTML = "<b><font color='SpringGreen'>查询成功！</font></b>" + "<br>" + "<b>BAN ID:</b>" + obj.blacklist[0].id + "<br>" + "<b>QQ号:</b>" + obj.blacklist[0].qq + "<br>" + "<b>记录日期:</b>" + obj.blacklist[0].date + "<br>" + "<b>有效期至:</b>" + obj.blacklist[0].validity + "<br>" + "<b>云黑名单等级:</b>" + obj.blacklist[0].level + "<br>" + "<b>事件记录:</b>" + obj.blacklist[0].note + "<font color='red'><b>【重度违规，建议提高警惕】</b></font>" + "<br>" + "<b>证实情况:</b>" + obj.blacklist[0].confirm + "<br>" + "<b>证据链接:</b>" + obj.blacklist[0].evidence + "<br>" + "<b>查询结果来自:</b>" + obj.blacklist[0].from;
             break;
             
     }
