9bbb80c7dfa10c6374fed46c7c7a09dfa5ec9853
fly6022 <2875752791@qq.com>
2020-05-02 17:47:53 +0800

    更新网站

--- ban.js
+++ ban.js
@@ -3,8 +3,6 @@
 
 var json
 
-search_value = document.getElementById('ebsban').value;
-
 window.onload = function blacklist_json () {
     var url = "blacklist.json"
     var request = new XMLHttpRequest();
@@ -15,7 +13,9 @@ window.onload = function blacklist_json () {
             json = eval(JSON.parse(request.responseText));
             for (let i = 0; i < json.blacklist.length; i++) {
                 switch (search_value) {
-            
+
+                    search_value = document.getElementById('ebsban').value;
+
                     case '0':
                     case 'test':
                         obj = JSON.parse(json); document.getElementById("ban").innerHTML = "<b><font color='SpringGreen'>查询成功！</font></b>" + "<br>" + "<b>BAN ID:</b>" + obj.blacklist[0].id + "<br>" + "<b>QQ号:</b>" + obj.blacklist[0].qq + "<br>" + "<b>记录日期:</b>" + obj.blacklist[0].date + "<br>" + "<b>有效期至:</b>" + obj.blacklist[0].validity + "<br>" + "<b>云黑名单等级:</b>" + obj.blacklist[0].level + "<br>" + "<b>事件记录:</b>" + obj.blacklist[0].note + "<font color='red'><b>【重度违规，建议提高警惕】</b></font>" + "<br>" + "<b>证实情况:</b>" + obj.blacklist[0].confirm + "<br>" + "<b>证据链接:</b>" + obj.blacklist[0].evidence + "<br>" + "<b>查询结果来自:</b>" + obj.blacklist[0].from;
